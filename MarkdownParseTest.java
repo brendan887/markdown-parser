798d0deddb3c4c1c8a45848ef53f4bfc16b52e93
brendan887 <mahbrendan@gmail.com>
2022-04-21 17:28:25 -0700

    Created a test

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -1,9 +1,21 @@
-import static org.junit.Assert.*;
+import static org.junit.Assert.*; // imports necessary libraries for junit tests
 import org.junit.*;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
 
 public class MarkdownParseTest {
+    @Test // specifies that the following method is a test
+    public void addition() { // new unit test method
+        assertEquals(2, 1+1); // checks whether the two arguments are equal
+    }
+
     @Test
-    public void addition() {
-        assertEquals(2, 1+1);
+    public void getLinksTest() throws IOException{
+        String fileName = "test-file.md";
+        List generatedList = MarkdownParse.getLinks(Files.readString(Path.of(fileName)));
+        List testList = List.of("https://something.com", "some-thing.html");
+        assertEquals(generatedList, testList);
     }
 }
